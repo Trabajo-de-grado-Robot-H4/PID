f98acc6085565909733a5d686674530a9ee772ea
CristianGoFI <74423403+CristianGoFI@users.noreply.github.com>
2020-12-08 18:03:45 -0500

    Update RNP_Encoder.py

--- RNP_Encoder.py
+++ RNP_Encoder.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 # license removed for brevity
+""" importar librerias necesarias """
 import rospy
-from std_msgs.msg import String
+import RPi.GPIO as GPIO # libreria para comunicacion de puestos GPIO de la raspberry
+import time             # libreria para obtener el tiempo 
 
+from geometry_msgs.msg import Pose # importamos el tipo de dato pose
+Enc=Pose()
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('Encoder', Pose, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        
+        
+        
+        rospy.loginfo(Enc)
+        pub.publish(Enc)
         rate.sleep()
 
 if __name__ == '__main__':
